b1729190823bd34611adee78da33fdcd42b37085
Brady Lumsden <blumsden007@gmail.com>
2024-05-10 10:29:17 -0700

    create sort functionality, style sort selector menu, replace items in itemList with sortedItems()

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,12 +1,56 @@
+import { useState } from "react";
 import EmptyState from "./EmptyState";
 import Item from "./Item";
+import Select from "react-select";
 
 export default function ItemList({ items, onToggle, onDelete }) {
+  const options = [
+    { value: "default", label: "Sort By Default" },
+    { value: "unpacked", label: "Sort By Unpacked" },
+    { value: "packed", label: "Sort By Packed" },
+  ];
+  const [sortBy, setSortBy] = useState("default");
+
+  const sortedItems = () => {
+    const itemsToSort = items;
+    if (sortBy.value === "default") {
+      return itemsToSort;
+    } else if (sortBy.value === "unpacked") {
+      return itemsToSort.sort((a, b) => {
+        return a.packed - b.packed;
+      });
+    } else if (sortBy.value === "packed") {
+      return itemsToSort.sort((a, b) => {
+        return b.packed - a.packed;
+      });
+    } else {
+      return itemsToSort;
+    }
+  };
   return (
     <ul className="item-list">
-      {/* <Select /> */}
-      {items.length > 0 ? (
-        items.map((item) => {
+      <Select
+        styles={{
+          container: (styles) => ({
+            ...styles,
+            padding: "15px 28px",
+            borderBottom: "1px solid rgba(0, 0, 0, 0.05)",
+            fontSize: "13px",
+          }),
+          menu: (styles) => ({
+            ...styles,
+            padding: "0px",
+            fontSize: "13px",
+            width: "90%",
+          }),
+        }}
+        // className="sorting"
+        options={options}
+        value={sortBy}
+        onChange={setSortBy}
+      />
+      {sortedItems().length > 0 ? (
+        sortedItems().map((item) => {
           return (
             <Item
               onDelete={onDelete}
