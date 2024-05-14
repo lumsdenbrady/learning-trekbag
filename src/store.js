ac088a70273f1d7f6c95a0c69459f44efdb9d637
Brady Lumsden <blumsden007@gmail.com>
2024-05-13 17:38:35 -0700

    work on adding a zustand store to the application, create addItem function, work on deleteItem function

---
+++ src/store.js
@@ -0,0 +1,33 @@
+import {create} from "zustand";
+
+export const useStore = ((set) => ({
+    items: [],
+    initialItems: [
+        {
+          id: 1,
+          text: "Phone Charger",
+          packed: true,
+        },
+        {
+          id: 2,
+          text: "Underwear",
+          packed: false,
+        },
+        {
+          id: 3,
+          text: "Good Vibes",
+          packed: false,
+        },
+      ],
+      addItem: (text)=> set((state)=>({
+
+        items: [...state.items,
+            {id : new Date().getTime(),
+                text,
+                packed: false,
+            }]
+    })),
+    deleteItem: (id) => set((state) =>({
+items: state.items.filter((item)=>item.id !== id)
+    }))
+}))
