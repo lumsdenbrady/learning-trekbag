5cd249d782555442a980abfb5e8e38d928bd4166
Brady Lumsden <blumsden007@gmail.com>
2024-05-15 19:05:44 -0700

    add some functions to store.js file

--- src/store.js
+++ src/store.js
@@ -1,33 +1,44 @@
-import {create} from "zustand";
+import { create } from "zustand";
 
-export const useStore = ((set) => ({
-    items: [],
-    initialItems: [
-        {
-          id: 1,
-          text: "Phone Charger",
-          packed: true,
-        },
-        {
-          id: 2,
-          text: "Underwear",
-          packed: false,
-        },
-        {
-          id: 3,
-          text: "Good Vibes",
-          packed: false,
-        },
+export const useStore = (set) => ({
+  items: [],
+  initialItems: [
+    {
+      id: 1,
+      text: "Phone Charger",
+      packed: true,
+    },
+    {
+      id: 2,
+      text: "Underwear",
+      packed: false,
+    },
+    {
+      id: 3,
+      text: "Good Vibes",
+      packed: false,
+    },
+  ],
+  addItem: (text) =>
+    set((state) => ({
+      items: [
+        ...state.items,
+        { id: new Date().getTime(), text, packed: false },
       ],
-      addItem: (text)=> set((state)=>({
-
-        items: [...state.items,
-            {id : new Date().getTime(),
-                text,
-                packed: false,
-            }]
-    })), 
-    deleteItem: (id) => set((state) =>({
-items: state.items.filter((item)=>item.id !== id)
-    }))
-}))
+    })),
+  deleteItem: (id) =>
+    set((state) => ({
+      items: state.items.filter((item) => item.id !== id),
+    })),
+  togglePacked: (id) =>
+    set((state) => ({
+      items: state.items.map((item) => {
+        if (item.id === id) {
+          return { ...item, packed: !item.packed };
+        } else {
+          return item;
+        }
+      }),
+    })),
+  deleteAllItems: () => set(() => ({ items: [] })),
+});
